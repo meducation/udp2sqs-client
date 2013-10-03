e39413a51f4e74c111a1902a891154151c983a1e
Jeremy Walker <jez.walker@gmail.com>
2013-10-03 14:18:52 +0100

    Refactor filenames and whitespace

--- src/udp2sqs-client.rb
+++ src/udp2sqs-client.rb
@@ -2,22 +2,22 @@ require 'socket'
 
 module Udp2sqs
 
-	class Client
+  class Client
 
     def initialize
-	  	@host, @port = "localhost", 9732
+      @host, @port = "localhost", 9732
     end
 
-	  def configure(host: "localhost", port: 9732)
-	  	@host, @port = host, port
-	  end
+    def configure(host: "localhost", port: 9732)
+      @host, @port = host, port
+    end
 
-	  def send(message)
-			s = UDPSocket.new
-			bytes_sent = s.send(message, 0, @host, @port)
-			return bytes_sent == message.bytesize
-	  end
+    def send(message)
+      s = UDPSocket.new
+      bytes_sent = s.send(message, 0, @host, @port)
+      return bytes_sent == message.bytesize
+    end
 
-	end
+  end
 
 end
