0391ebd1323a561eb91f467418514a59707f392e
MalcyL <malcolm@landonsonline.me.uk>
2013-10-04 10:50:42 +0100

    Added coveralls.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in udp2sqs_client.gemspec
 gemspec
+
+gem 'coveralls', require: false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,7 @@ gem "minitest"
 require "minitest/autorun"
 require "minitest/mock"
 require "minitest/stub_any_instance"
+require 'coveralls'
+Coveralls.wear!
+
 require_relative "../lib/udp2sqs_client"
