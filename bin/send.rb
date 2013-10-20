b1d322d688f9ba529b4dce4fed11373049b73e8f
MalcyL <malcolm@landonsonline.me.uk>
2013-10-20 14:38:00 +0100

    Modified bin/send.rb to send a message in the new propono format.

--- bin/send.rb
+++ bin/send.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/udp2sqs_client/client.rb'
-session_id = ARGV[0]
+topic = ARGV[0]
+session_id = ARGV[1]
 
 client = Udp2sqsClient::Client.new(host: 'localhost')
-client.send_message("{\"action\" : \"pageview\", \"time\" : \"\", \"payload\" : { \"session_id\" : \"#{session_id}\", \"user_id\" : null, \"premium\" : false } }")
+client.send_message("{\"topic\" : \"#{topic}\", \"message\" : {\"action\" : \"pageview\", \"time\" : \"\", \"payload\" : { \"session_id\" : \"#{session_id}\", \"user_id\" : null, \"premium\" : false } } }")
